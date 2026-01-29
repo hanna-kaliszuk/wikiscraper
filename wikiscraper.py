5066596a8dbc37f227c43804548a16bb0a8a36aa
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2026-01-29 19:36:40 +0100

    get_table poczatek

--- wikiscraper.py
+++ wikiscraper.py
@@ -4,16 +4,19 @@
 import os
 
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
+from io import StringIO
 
 
 class WikiScraper:
-    def __init__(self, base_url, phrase, use_local_file=False, local_file_path=None):
+    def __init__(self, base_url, phrase, use_local_file=False, local_file_path=None, first_row_is_header=False):
         # konstruktor, który umożliwia przechowywanie swojego stanu.
         self.base_url = base_url
         self.phrase = phrase
         self.use_local_file = use_local_file
         self.local_file_path = local_file_path
+        self.first_row_is_header = first_row_is_header
         self.soup = None  # bo na razie nie ma sparsowanego HTMLa
 
     def _get_url(self):
@@ -84,13 +87,47 @@ class WikiScraper:
 
         return "No summary available."
 
+    def get_table(self, table_number, first_row_is_header=False):
+        if not self.soup:
+            if not self.fetch_data():
+                return None
+        # szukamy głównego kontenera z tekstem (w divie o klasie mw-parser-output)
+        content_div = self.soup.find('div', class_='mw-parser-output')
+
+        # jezeli nie ma klasy, szukaj po id
+        if not content_div:
+            content_div = self.soup.find('div', id='mw-content-text')
+
+        if not content_div:
+            return "Could not find content on the page."
+
+        tables = content_div.find_all('table')
+
+        if table_number > len(tables):
+            return f"Table number {table_number} does not exist on the page."
+
+        selected_table = tables[table_number - 1]
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     base_url = "https://bulbapedia.bulbagarden.net"
-    scraper = WikiScraper(base_url, "Team Rocket")
+    scraper = WikiScraper(base_url, "Type")
     print(f"Fetching summary for {scraper.phrase} from {base_url}")
     summary = scraper.get_summary()
+    table = scraper.get_table(table_number=2, first_row_is_header=True)
 
     print("#" * 30)
     print("SUMMARY:")
     print(summary)
+    print("#" * 30)
+
+    print("#" * 30)
+    print("TABLE:")
+    print(table)
     print("#" * 30)
