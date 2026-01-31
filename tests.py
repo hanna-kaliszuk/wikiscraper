7dcca8cba3d332ecafb8d57d3df4eade4890b1dc
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2026-01-31 12:03:19 +0100

    komentarze

--- tests.py
+++ tests.py
@@ -1,20 +1,47 @@
+"""
+UNIT TEST MODULE
+
+This module contains unit tests for the WikiScraper class.
+
+Verifies:
+    - URL generation
+    - content parsing
+    - table extraction and conversion to DataFrame
+
+Usage:
+    Run via command line: python tests.py
+
+Author:
+    Hanna Kaliszuk, January 2026
+"""
+
+# --- standard library imports ---
 import unittest
+
+# -- third-party imports ---
 import pandas as pd
 from bs4 import BeautifulSoup
-from wiki_scraper import WikiScraper
+
+# --- local imports ---
+from wiki_scraper import WikiScraper, table_to_dataframe
+
 
 class TestWikiScraper(unittest.TestCase):
     def setUp(self):
-        # uruchamia się przed kazdym testem i przygotowuje swiezy obiekt
+        """
+        Setting up the test environment.
+        Initializes a WikiScraper instance before every test method.
+        """
         self.scraper = WikiScraper("Test Phrase")
 
     def test_get_url(self):
-        # sprawdz, czy faza ze spacjami zmienia sie na oczekiwanu URL
+        """Tests if the URL is generated correctly from the given phrase."""
         self.scraper.phrase = "Team Rocket"
-        expected_udl = "https://bulbapedia.bulbagarden.net/wiki/Team_Rocket"
-        self.assertEqual(self.scraper._get_url(), expected_udl)
+        expected_url = "https://bulbapedia.bulbagarden.net/wiki/Team_Rocket"
+        self.assertEqual(self.scraper._get_url(), expected_url)
 
     def test_get_summary_offline(self):
+        """Tests if the summary is being extracted correctly using an offline HTML structure."""
         html_content = """
         <div class="mw-parser-output">
             <p></p>
@@ -31,6 +58,7 @@ class TestWikiScraper(unittest.TestCase):
         self.assertEqual(result, expected_text)
 
     def test_get_internal_links(self):
+        """Tests the extraction of internal Wiki links"""
         html_content = """
         <div class="mw-parser-output">
             <a href="/wiki/Pikachu">Link 1</a>
@@ -51,6 +79,7 @@ class TestWikiScraper(unittest.TestCase):
         self.assertNotIn("https://google.com", links)
 
     def test_table_parsing(self):
+        """Tests the convertion of HTML table to DataFrame."""
         html_content = """
         <table>
             <tr><th>Col1</th><th>Col2</th></tr>
@@ -62,13 +91,14 @@ class TestWikiScraper(unittest.TestCase):
         soup = BeautifulSoup(html_content, "html.parser")
         table = soup.find("table")
 
-        df = self.scraper._table_to_dataframe(table, first_row_is_header=True)
+        df = table_to_dataframe(table, first_row_is_header=True)
 
         self.assertIsInstance(df, pd.DataFrame)
-        self.assertEqual(len(df), 2) # 2 wiersze danych
+        self.assertEqual(len(df), 2) # should have 2 data rows
 
-        self.assertEqual(df.iloc[0, 0], 10) # pierwszy wiersz pierwsza kolumna danych
-        self.assertEqual(df.index[0], 'A') # indeks pierwszego wiesza
+        self.assertEqual(df.iloc[0, 0], 10)
+        self.assertEqual(df.index[0], 'A')
+
 
 if __name__ == '__main__':
     unittest.main()
